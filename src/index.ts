45baf7d5f65bcfc265638f84eee7c1c0b88c5b77
buhi <yinshang1992@gmail.com>
2020-12-31 15:41:00 +0800

    v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { FormControl } from "./form-control";
-export { FormControlGroup } from "./form-control-group";
-export { FormControlList } from "./form-control-list";
-export * from "./types";
+export { FormControl } from "./form-control"
+export { FormControlGroup } from "./form-control-group"
+export { FormControlList } from "./form-control-list"
+export type { AbstractControl, ValidationInfo, Validator, FormControlOptions } from "./types"
